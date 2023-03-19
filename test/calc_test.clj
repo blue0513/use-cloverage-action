b0c4f301fe349622a47d9353ad65bee6c4ebcb0c
Taiju Aoki <aokitaiju0513@gmail.com>
2023-03-19 20:12:40 +0900

    run ci

---
+++ test/calc_test.clj
@@ -0,0 +1,11 @@
+(ns calc-test
+  (:require [clojure.test :refer :all]
+            [calc :as c]))
+
+(deftest sum-test
+  (is (= 3 (c/sum 1 2)))
+  (is (= 3 (c/sum 2 1))))
+
+(deftest multiply-test
+  (is (= 2 (c/multiply 1 2)))
+  (is (= 2 (c/multiply 2 1))))

5289ad9cac273b8d63dc712c38ccc81be3f16902
Taiju Aoki <aokitaiju0513@gmail.com>
2023-03-19 20:58:49 +0900

    run ci

--- test/calc_test.clj
+++ test/calc_test.clj
@@ -6,6 +6,6 @@
   (is (= 3 (c/sum 1 2)))
   (is (= 3 (c/sum 2 1))))
 
-(deftest multiply-test
-  (is (= 2 (c/multiply 1 2)))
-  (is (= 2 (c/multiply 2 1))))
+;; (deftest multiply-test
+;;   (is (= 2 (c/multiply 1 2)))
+;;   (is (= 2 (c/multiply 2 1))))
